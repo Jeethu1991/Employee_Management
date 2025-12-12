8746ab254d5658f7e8fa53126ed1b26e36a23ef4
Jeethu1991 <jeethupjoy@gmail.com>
2025-12-12 10:18:36 +0530

    Initial commit: Created base ASP.NET MVC/Web API project with default configuration

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,80 @@
+﻿using EmployeeManagement.Data;
+using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmployeeManagement.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepository _repo = new EmployeeRepository();
+
+        public ActionResult Index()
+        {
+            var list = _repo.GetAll();
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult AjaxCreate(Employee model)
+        {
+            Debug.WriteLine("AjaxCreate called");
+            if (ModelState.IsValid)
+            {
+                var rows = _repo.Save(model);
+                return Json(new { success = rows > 0 });
+            }
+
+            return Json(new { success = false, errors = ModelState });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var e = _repo.GetById(id);
+            if (e == null) return HttpNotFound();
+            return View(e);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employee model)
+        {
+            if(ModelState.IsValid)
+            {
+                _repo.Update(model);
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var e = _repo.GetById(id);
+            if (e == null) return HttpNotFound();
+            return View(e);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            _repo.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public JsonResult GetAllJson()
+        {
+            var list = _repo.GetAll();
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Data/EmployeeManagement.cs
@@ -0,0 +1,123 @@
+﻿using Antlr.Runtime;
+using EmployeeManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Xml.Linq;
+
+
+namespace EmployeeManagement.Data
+{
+    public class EmployeeRepository
+    {
+        private readonly string _connStr = ConfigurationManager.ConnectionStrings["EmployeeDB"].ConnectionString;
+
+        public List<Employee> GetAll()
+        {
+            var list = new List<Employee>();
+            using(var con = new SqlConnection(_connStr))
+            using(var cmd = new SqlCommand("GetAllEmployees",con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                using (var r = cmd.ExecuteReader())
+                {
+                    while(r.Read())
+                    {
+                        list.Add(new Employee
+                        {
+                            Id = Convert.ToInt32(r["Id"]),
+                            Name = r["Name"].ToString(),
+                            Email = r["Email"].ToString(),
+                            Department = r["Department"].ToString(),
+                            Salary = Convert.ToDecimal(r["Salary"])
+                        });
+                    }
+                }
+            }
+            return list;
+        }
+
+        public Employee GetById(int id)
+        {
+            using (var con = new SqlConnection(_connStr))
+            using (var cmd = new SqlCommand("GetEmployeeById", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                using(var r = cmd.ExecuteReader())
+                {
+                    if(r.Read())
+                    {
+                        return new Employee
+                        {
+                            Id = Convert.ToInt32(r["Id"]),
+                            Name = r["Name"].ToString(),
+                            Email = r["Email"].ToString(),
+                            Department = r["Department"].ToString(),
+                            Salary = Convert.ToDecimal(r["Salary"])
+                        };
+                    }
+                }
+            }
+            return null;
+        }
+
+        public int Save(Employee e)
+        {
+            Debug.WriteLine("CONN STRING = " + _connStr);
+
+            using (var con = new SqlConnection(_connStr))
+            using (var cmd = new SqlCommand("SaveEmployee", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Name", e.Name ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Email", e.Email ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Department", e.Department ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Salary", e.Salary);
+
+                con.Open();
+                var result = cmd.ExecuteNonQuery();
+                Debug.WriteLine("Rows inserted: " + result);
+                return result;
+
+                //Name, Email, Department, Salary
+            }
+        }
+
+        public bool Update(Employee e)
+        {
+            using (var con = new SqlConnection(_connStr))
+            using (var cmd = new SqlCommand("UpdateEmployee", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", e.Id);
+                cmd.Parameters.AddWithValue("@Name", e.Name ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Email", e.Email ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Department", e.Department ?? (object)DBNull.Value);
+                cmd.Parameters.AddWithValue("@Salary", e.Salary);
+                con.Open();
+                return cmd.ExecuteNonQuery() > 0;
+            }
+        }
+
+        public bool Delete(int id)
+        {
+            using (var con = new SqlConnection(_connStr))
+            using (var cmd = new SqlCommand("DeleteEmployee", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@Id", id);
+                con.Open();
+                return cmd.ExecuteNonQuery() < 0;
+            }
+        }
+    }
+}
